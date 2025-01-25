845d7874ae751794316d516af1f8fe8a4807d910
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2025-01-25 16:16:27 +0000

    Send addNode down websocket

--- frontend/src/lib/components/Graph.svelte
+++ frontend/src/lib/components/Graph.svelte
@@ -1,97 +1,105 @@
 <script>
-	import { createCounter } from "$lib/utils/Counter.svelte.js";
-    import Graph from "graphology";
-    import { getWebSocket } from "$lib/websocket.js"
+	import { createCounter } from '$lib/utils/Counter.svelte.js';
+	import Graph from 'graphology';
+	import { getWebSocket } from '$lib/websocket.js';
 
-    
-    const socket = getWebSocket();
+	const socket = getWebSocket();
 
-    // graphContainer is the div containing the graph
-    let graphContainer;
-    let graph = $state(new Graph());
+	// graphContainer is the div containing the graph
+	let graphContainer;
+	let graph = $state(new Graph());
 
-    let nodeCounter = createCounter()
-    
-    // TODO - get state from server
-    let graphJSON = $derived(JSON.stringify(graph.export(), null, 2))
+	let nodeCounter = createCounter();
 
-    function addNode() {
-        // Server call
-        graph.addNode(nodeCounter.next(), { label: `Node ${nodeCounter.count}`, x: 3, y: 2, size: 30, color: "green" });
-    }
+	// TODO - get state from server
+	let graphJSON = $derived(JSON.stringify(graph.export(), null, 2));
 
-    function refreshState() {
-        /**
-         * Problem:
-         * Svelte is not picking up that the graph object has changed 
-         * with addNode and addEdge calls.
-         * 
-         * Solution:
-         * Re-assign graph to tell svelte that the state has changed
-         * and to re-render the element in the UI.
-         * 
-         * TODO:
-         * Better long term solution
-         */
-        const graphTmp = graph;
-        graph = null
-        graph = graphTmp
-    }
+	function addNode() {
+		socket.send({
+			method: 'addNode',
+			payload: { label: `Node ${nodeCounter.count}`, x: 3, y: 2, size: 30, color: 'green' }
+		});
+	}
 
-    $effect(async () => {
-        if (typeof window !== "undefined") {
+	function refreshState() {
+		/**
+		 * Problem:
+		 * Svelte is not picking up that the graph object has changed
+		 * with addNode and addEdge calls.
+		 *
+		 * Solution:
+		 * Re-assign graph to tell svelte that the state has changed
+		 * and to re-render the element in the UI.
+		 *
+		 * TODO:
+		 * Better long term solution
+		 */
+		const graphTmp = graph;
+		graph = null;
+		graph = graphTmp;
+	}
 
-            /**
-             * Problem:
-             * Importing Sigma at the top level gives the following error:
-             * 
-             * ReferenceError: WebGL2RenderingContext is not defined
-             * 
-             * Temporary Solution:
-             * Dynamically import Sigma - this is because it depends on WebGL2RenderingContext
-             * which is a browser-specific API (not available on the server).
-             * 
-             * TODO:
-             * Better long term solution
-             */
-            const { Sigma } = await import("sigma");
-            
-            console.log("Graph state updated")
-            graph.addNode(nodeCounter.next(), { label: `Node ${nodeCounter.count}`, x: 0, y: 0, size: 10, color: "blue" });
-            graph.addNode(nodeCounter.next(), { label: `Node ${nodeCounter.count}`, x: 1, y: 1, size: 20, color: "red" });
-            graph.addEdge(1, "2", { size: 5, color: "purple" });
+	$effect(async () => {
+		if (typeof window !== 'undefined') {
+			/**
+			 * Problem:
+			 * Importing Sigma at the top level gives the following error:
+			 *
+			 * ReferenceError: WebGL2RenderingContext is not defined
+			 *
+			 * Temporary Solution:
+			 * Dynamically import Sigma - this is because it depends on WebGL2RenderingContext
+			 * which is a browser-specific API (not available on the server).
+			 *
+			 * TODO:
+			 * Better long term solution
+			 */
+			const { Sigma } = await import('sigma');
 
-            refreshState()  // TODO remove need for this call
+			console.log('Graph state updated');
+			graph.addNode(nodeCounter.next(), {
+				label: `Node ${nodeCounter.count}`,
+				x: 0,
+				y: 0,
+				size: 10,
+				color: 'blue'
+			});
+			graph.addNode(nodeCounter.next(), {
+				label: `Node ${nodeCounter.count}`,
+				x: 1,
+				y: 1,
+				size: 20,
+				color: 'red'
+			});
+			graph.addEdge(1, '2', { size: 5, color: 'purple' });
 
-            // Initialize Sigma with the graphology graph and container
-            // Insert the graph in the graphContainer
-            new Sigma(graph, graphContainer);
-        }
-    });
+			refreshState(); // TODO remove need for this call
+
+			// Initialize Sigma with the graphology graph and container
+			// Insert the graph in the graphContainer
+			new Sigma(graph, graphContainer);
+		}
+	});
 </script>
 
-<h1 class="text-4xl p-8">Quorum</h1>
+<h1 class="p-8 text-4xl">Quorum</h1>
 
 <!-- TODO: handle a11y properly -->
 <!-- svelte-ignore a11y_click_events_have_key_events -->
 <!-- svelte-ignore a11y_no_static_element_interactions -->
-<div 
-    bind:this={graphContainer}
-    onclick={addNode}
-    class="graph-container"
->
-    <!-- Graph will be inserted here -->
+<div bind:this={graphContainer} onclick={addNode} class="graph-container">
+	<!-- Graph will be inserted here -->
 </div>
 
-<h1 class="text-4xl p-8">Graph JSON</h1>
+<h1 class="p-8 text-4xl">Graph JSON</h1>
 
 <div class="p-8">
-    <pre>{graphJSON}</pre>
+	<pre>{graphJSON}</pre>
 </div>
 
 <style>
-    .graph-container {
-        width: 100%; 
-        height: 400px;
-    }
+	.graph-container {
+		width: 100%;
+		height: 400px;
+	}
 </style>
--- frontend/src/lib/websocket.js
+++ frontend/src/lib/websocket.js
@@ -1,5 +1,5 @@
 const BACKEND = "100.95.231.119:8000"
-const BASE_ENDPOINT=`ws://${BACKEND}/api/v1/ws/`
+// const BASE_ENDPOINT=`ws://${BACKEND}/api/v1/ws/`
 const RPC_ENDPOINT=`ws://${BACKEND}/api/v1/ws/rpc`
 
 let socket;
@@ -7,15 +7,14 @@ let socket;
 export function getWebSocket() {
   if (!socket) {
     console.log("Initialising websocket connection to backend.")
-    socket = new WebSocket(BASE_ENDPOINT);
+    socket = new WebSocket(RPC_ENDPOINT);
 
     socket.onopen = () => {
       console.log("WebSocket connection established.");
-      socket.send("test")
     };
 
     socket.onmessage = (event) => {
-      console.log("Message from server:", event.data);
+      console.log("Message from backend:", event.data);
     };
 
     socket.onerror = (error) => {
@@ -23,7 +22,7 @@ export function getWebSocket() {
     };
 
     socket.onclose = (event) => {
-      console.log("WebSocket connection closed:", event.reason);
+      console.log("WebSocket connection to backend closed:", event.reason);
       // Optionally, you can handle reconnection logic here
     };
 
