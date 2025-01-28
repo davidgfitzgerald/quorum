1f86d57f7e0c6670bafcb7988ac4bf457f100c08
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2025-01-28 19:49:24 +0000

    Point eslint to ../.gitignore

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,7 +4,7 @@ import { includeIgnoreFile } from '@eslint/compat';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
-const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
+const gitignorePath = fileURLToPath(new URL('../.gitignore', import.meta.url));
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
