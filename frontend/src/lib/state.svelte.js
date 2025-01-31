6c10be4acfc29e7fdc543df7f7b11e0d08d1da3a
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2025-01-31 08:46:39 +0000

    Add example state

---
+++ frontend/src/lib/state.svelte.js
@@ -0,0 +1,53 @@
+/**
+ * The file contains the client-side state object that
+ * is updated whenever new state is retrieved from the
+ * backend.
+ * 
+ * For now, we just return some example state. Eventually
+ * this will be retrieved from the backend.
+ */
+const example_state = {
+	state: {
+		options: {
+			type: 'mixed',
+			multi: false,
+			allowSelfLoops: true
+		},
+		attributes: {},
+		nodes: [
+			{
+				key: '1',
+				attributes: {
+					label: 'Node 1',
+					x: 0,
+					y: 0,
+					size: 10,
+					color: 'blue'
+				}
+			},
+			{
+				key: '2',
+				attributes: {
+					label: 'Node 2',
+					x: 1,
+					y: 1,
+					size: 20,
+					color: 'red'
+				}
+			}
+		],
+		edges: [
+			{
+				key: 'geid_192_0',
+				source: '1',
+				target: '2',
+				attributes: {
+					size: 5,
+					color: 'purple'
+				}
+			}
+		]
+	}
+}
+
+export const graphState = $state(example_state);
