1014f27b27bf0d623be6910c79ac8143b952536e
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2025-02-08 12:22:36 +0000

    Rename manager import to connections

--- backend/quorum/api/routes/ws.py
+++ backend/quorum/api/routes/ws.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 from starlette import status
 
 from quorum.core.state import graph
-from quorum.core.connection import manager
+from quorum.core.connection import manager as connections
 
 router = APIRouter(
     tags=["ws"], 
@@ -12,21 +12,21 @@ router = APIRouter(
 
 @router.websocket("/")
 async def websocket_echo(websocket: WebSocket):
-    client_id = await manager.connect(websocket)
+    client_id = await connections.connect(websocket)
 
     try:
         await websocket.send_json({"client_id": str(client_id)})
         while True:
             data = await websocket.receive_text()
-            await manager.broadcast(f"Client #{client_id} says: {data}", client_id)
+            await connections.broadcast(f"Client #{client_id} says: {data}", client_id)
     except WebSocketDisconnect:
-        await manager.disconnect(client_id)
-        await manager.broadcast(f"Client #{client_id} left the chat", client_id)
+        await connections.disconnect(client_id)
+        await connections.broadcast(f"Client #{client_id} left the chat", client_id)
 
 
 @router.websocket("/rpc")
 async def websocket_rpc(websocket: WebSocket):
-    client_id = await manager.connect(websocket)
+    client_id = await connections.connect(websocket)
     state = graph.model_dump()
     exit_code = status.WS_1000_NORMAL_CLOSURE
     try:
@@ -66,4 +66,4 @@ async def websocket_rpc(websocket: WebSocket):
         # code 1011 chosen from https://www.rfc-editor.org/rfc/rfc6455.html#section-7.4.1
         exit_code=status.WS_1011_INTERNAL_ERROR
     finally:
-        await manager.disconnect(client_id, code=exit_code)
+        await connections.disconnect(client_id, code=exit_code)
